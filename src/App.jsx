0564eeae9dd2e689593d705fbea6c73000efec95
Farhan Ahmed <farhanf7n@Farhans-MacBook-Pro.local>
2022-03-17 10:58:36 +0500

    structured files

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
-
 const App = () => {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import App from './App';
 import './index';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(<App/>,document.querySelector("#root"))
